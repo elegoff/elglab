9f66aee5c712c1dd4a0059642c85616d3467a398
Eric Le Goff <elegoff@gmail.com>
2015-10-01 10:01:38 +0200

    Initial commit

---
+++ src/elglab/handler.clj
@@ -0,0 +1,17 @@
+(ns elglab.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :refer [run-jetty]]))
+
+(defroutes app-routes
+  (GET "/" [] "<h1>Hi Compojure</h1>")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
+
+(defn -main [& args]
+  (let [ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")
+        port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))]
+    (run-jetty app {:host ip :port port})))
